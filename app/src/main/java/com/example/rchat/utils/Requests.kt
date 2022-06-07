80088f2ba0dc13a7e9102ee53c0a7bcbc2f916f1
ValikSilchenko <valik.silchenko@gmail.com>
2022-06-07 18:17:19 +0300

    try 2

--- app/src/main/java/com/example/rchat/utils/Requests.kt
+++ app/src/main/java/com/example/rchat/utils/Requests.kt
@@ -1,10 +1,8 @@
 package com.example.rchat.utils
 
 import android.os.StrictMode
-import okhttp3.FormBody
+import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import okio.IOException
 
 
@@ -14,7 +12,6 @@ class Requests {
 
     fun post(data: Map<String, String>, url: String): String {
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
-
         val httpBuilder = url.toHttpUrlOrNull() ?: throw IOException("Bad url")
         val dataToSend = FormBody.Builder()
         data.forEach { (key, value) ->
@@ -33,7 +30,7 @@ class Requests {
         }
     }
 
-    fun get(data: Map<String,String>, url: String): String {
+    fun get(data: Map<String, String>, url: String): String {
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
 
         val httpBuilder = url.toHttpUrlOrNull() ?: throw IOException("Bad url")
@@ -53,4 +50,23 @@ class Requests {
             return response.body!!.string()
         }
     }
+
+//    val response = client.newCall(
+//        request
+//            .get()
+//            .url(queryData.build())
+//            .build()
+//    ).enqueue(object : Callback {
+//        override fun onFailure(call: Call, e: IOException) {
+//            e.printStackTrace()
+//        }
+//
+//        override fun onResponse(call: Call, response: Response) {
+//            response.use {
+//                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+//
+//                ChatSingleton.func(response.body!!.string())
+//            }
+//        }
+//    })
 }
