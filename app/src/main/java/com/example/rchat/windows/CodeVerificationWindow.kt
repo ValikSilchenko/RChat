35af47f8f55bff4d8d5002103077147a684f5d81
KimYuriy <komrad.kim-j@yandex.ru>
2022-09-21 11:01:25 +0300

    - Added 2 new windows for code verification and for new password.

---
+++ app/src/main/java/com/example/rchat/windows/CodeVerificationWindow.kt
@@ -0,0 +1,12 @@
+package com.example.rchat.windows
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rchat.R
+
+class CodeVerificationWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.code_verification_window)
+    }
+}
--- app/src/main/java/com/example/rchat/windows/ResetPasswordWindow.kt
+++ app/src/main/java/com/example/rchat/windows/ResetPasswordWindow.kt
@@ -2,11 +2,7 @@ package com.example.rchat.windows
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.rchat.R
 import com.example.rchat.utils.ChatFunctions
 
@@ -17,25 +13,6 @@ class ResetPasswordWindow : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reset_password_window)
-
-        val backBtn: ImageButton = findViewById(R.id.RPW_BackBtn)
-        val sendEmailBtn: Button = findViewById(R.id.RPW_SendEmailBtn)
-        val sendCodeBtn: Button = findViewById(R.id.RPW_SendCodeBtn)
-        val sendPasswordBtn: Button = findViewById(R.id.RPW_SendNewPasswordBtn)
-        val enterEmailED: EditText = findViewById(R.id.RPW_EmailED)
-        val enterCodeED: EditText = findViewById(R.id.RPW_CodeED)
-        val enterNewPasswordED: EditText = findViewById(R.id.RPW_NewPasswordED)
-        val repeatNewPasswordED: EditText = findViewById(R.id.RPW_RepeatNewPasswordED)
-        val acceptCodeContainer: ConstraintLayout = findViewById(R.id.RPW_AcceptCodeContainer)
-        val newPasswordContainer: ConstraintLayout = findViewById(R.id.RPW_NewPasswordContainer)
-        var isEmailEntered = false
-        var isCodeEntered = false
-
-        /* Нажатие кнопки выхода назад
-        */
-        backBtn.setOnClickListener {
-            openNewWindow(AuthorizationWindow::class.java)
-        }
     }
 
     /* Функция открытия нового окна
